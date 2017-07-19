79c72330ce92700788f82c870cbd98cd72240957
Zachary <zkeesh@gmail.com>
2017-07-19 16:51:26 -0500

    header

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //  MONGOOSE
 var mongoose = require('mongoose');
 var db = mongoose.connection;
-mongoose.connect("mongodb://localhost/cryptonetnews");
+mongoose.connect("mongodb://localhost/crypto-news");
 mongoose.Promise = Promise;
 
 //  EXPRESS
@@ -15,6 +15,18 @@ app.set('view engine', 'hbs');
 
 // IMPORT THE ARTICLE MODEL
 var Article = require("./models/article.js");
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get('/about', function(req,res){
+  res.sendFile(__dirname + "/public/about.html");
+})
+
+app.get('/submit', function(req,res){
+  res.sendFile(__dirname + "/public/submit.html");
+})
 
 // Listen on the port
 app.listen(3000, function() {
